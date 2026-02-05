0c3ff9c8b1d1f084e43f9eb14e10a3517b952c4a
Meltem <63255967+meltemktn@users.noreply.github.com>
2026-02-06 00:14:07 +0300

    Add files via upload

---
+++ Project_2.sh
@@ -0,0 +1,23 @@
+  GNU nano 7.2                                                             project_2.sh                                                                       
+#!/bin/zsh
+
+conda activate base
+conda create -n funtools
+conda deactivate
+conda activate funtools
+conda install conda-forge::pyfiglet
+pyfiglet Meltem
+conda conda config --add channels defaults
+conda config --add channels bioconda
+conda config --add channels conda-forge
+conda install bioconda::bwa
+conda install bioconda::blast 
+conda install bioconda::samtools
+conda install bioconda::bedtools 
+conda install bioconda::spades
+conda install bioconda::bcftools 
+conda install bioconda::fastp 
+conda install bioconda::multiqc
+
+
+
